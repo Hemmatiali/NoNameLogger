d500ad3a1d293de968a86d245a183a2ce6f6b9cb
Hemmatiali <Hematialiii@gmail.com>
2026-02-08 22:00:22 +0330

    feat(project): init project.

---
+++ NoNameLogger/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NoNameLogger;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting...");
+        await Task.Delay(1000);
+        Console.WriteLine("Done...");
+    }
+}
