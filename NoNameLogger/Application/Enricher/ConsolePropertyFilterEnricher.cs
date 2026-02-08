d00dc9fbb17e893e3ce612c6a78afaa8a093bdd6
Hemmatiali <Hematialiii@gmail.com>
2026-02-08 22:29:44 +0330

    feat(enricher): add Serilog enrichers for scope context and console property filtering

---
+++ NoNameLogger/Application/Enricher/ConsolePropertyFilterEnricher.cs
@@ -0,0 +1,24 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace NoNameLogger.Application.Enricher;
+
+public sealed class ConsolePropertyFilterEnricher : ILogEventEnricher
+{
+    private static readonly HashSet<string> ExcludedKeys = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "Application",
+        "Environment",
+        "SourceContext"
+    };
+
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+
+        foreach (var key in ExcludedKeys)
+        {
+            logEvent.RemovePropertyIfPresent(key);
+        }
+    }
+}
